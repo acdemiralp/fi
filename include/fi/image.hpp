88df6f9bdabe13adf291c1e9fd72a842a973d96d
demiralp <demiralp@vr.rwth-aachen.de>
2018-01-09 17:45:16 +0100

    Added rule of five constructor/destructor/operators to image.

--- include/fi/image.hpp
+++ include/fi/image.hpp
@@ -2,7 +2,6 @@
 #define FI_IMAGE_HPP_
 
 #include <FreeImage.h>
-
 #define FREEIMAGE_COLORORDER FREEIMAGE_COLORORDER_RGB
 
 #include <fi/filter.hpp>
@@ -13,9 +12,15 @@ namespace fi
 class image
 {
 public:
+  image           ()                   = default;
+  image           (const image&  that) = default;
+  image           (      image&& temp) = default;
+  virtual ~image  ()                   = default;
+  image& operator=(const image&  that) = default;
+  image& operator=(      image&& temp) = default;
 
 protected:
-
+  FIBITMAP* native_;
 };
 }
 
