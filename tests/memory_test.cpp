5fa5346e64bd6c3f7a4083c5e2b8f440de61c928
demiralp <demiralp@vr.rwth-aachen.de>
2018-01-11 18:49:47 +0100

    Fixed memory test.

--- tests/memory_test.cpp
+++ tests/memory_test.cpp
@@ -7,7 +7,7 @@ TEST_CASE("Memory is tested.", "[memory]")
   std::vector<std::int16_t> block {0, 5, 10, 15, 20, 25};
   fi::memory                memory(fi::span<std::int16_t>{block.data(), block.size()});
 
-  REQUIRE(memory.span<std::int16_t>().data == block.data());
+  REQUIRE(memory.data<std::int16_t>().data == block.data());
   REQUIRE(memory.read<std::int16_t>(block.size()) == block);
   REQUIRE(memory.seek(sizeof std::int16_t));
   REQUIRE(memory.tell() == sizeof std::int16_t);
